bf9efb6845159f41c3b53529ebfbba853f692373
Virgil Martinez <virgilmartinez@gmail.com>
2018-03-13 15:40:39 -0500

    Removed an unnecessary action call.
    The button was removed in the current build so I removed the unnecessary action method.

--- iOU/ViewController.swift
+++ iOU/ViewController.swift
@@ -21,34 +21,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
-    
-    @IBAction func callBtn(_ sender: UIButton) {
-        let alert = UIAlertController(
-            //change this**
-            title: "Tell My ass to call you",
-            //this**
-            message: "When you just want me to call XD",
-            preferredStyle: .actionSheet)
-        
-        let messageComposeVC = messageComposer.configuredMessageComposeViewController()
-        
-        //Text message body**
-        messageComposeVC.body = "(IOU) Hey FOOL Call me right meow"
-        
-        //Alert
-        //Use
-        let action = UIAlertAction(title: "Use Now", style: .default, handler: {
-            action in
-            //Go to msg view controller
-            self.present(messageComposeVC, animated: true, completion: nil)
-        })
-        alert.addAction(action)
-        //Don't use
-        let action1 = UIAlertAction(title: "Not Now", style: .destructive, handler: nil)
-        alert.addAction(action1)
-        present(alert, animated: true, completion: nil)
-        
-    }
-    
 }
-
